d3e6559ea474669c6eae6f584a3c3b39fd38086b
LilChanclas <rosariohumbertozz@gmail.com>
2026-02-08 22:59:15 -0700

    bosquejo de homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center">
+    <main className="min-h-screen flex flex-col justify-top items-start">
+      <div className="min-w-screen flex border-b border-gray-500 bg-white">
+        <Link href="/stakeholders" className="flex items-center gap-3 text-gray-600
+            text-xl hover:text-purple-700 transition-colors px-15 font-semibold">
+              Stakeholders
+        </Link>
+
+        <Link href="/procesos" className="flex items-center gap-3 text-gray-600
+            text-xl hover:text-purple-700 transition-colors px-15 font-semibold">
+              Procesos
+        </Link>
+
+      </div>
+
+      <h1> AQUI VA UN RESUMEN DEL PROYECTO ACTUAL, PERO NO SE COMO HACERLO DINAMICO EN ESTE MOMENTO </h1>
+
     </main>
+
   );
 }

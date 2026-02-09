c83061ba9d50be2ff771012fef440e361cb4bb9d
LilChanclas <rosariohumbertozz@gmail.com>
2026-02-08 20:29:11 -0700

    componente dropdown
    componente dropdown e integracion dentro de la navbar

---
+++ app/components/Dropdown.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, {useState} from "react";
+import Link from "next/link";
+import { MenuItem} from "./Navbar"
+import { HiOutlineChevronDown } from "react-icons/hi";
+
+interface Props {
+    item: MenuItem;
+}
+
+export default function Dropdown(props: Props){
+    const { item } = props;
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const MenuItems = item?.children ? item.children : [];
+
+    const toggle = () => {
+        setIsOpen(old => !old);
+    }
+
+    const transClass = isOpen ? "flex" : "hidden";
+
+    return (
+        <>
+            <div className="relative">
+                <button className="flex items-center gap-2 text-purple-700 font-semibold text-lg transition duration-300 hover:scale-110"
+                onClick={toggle}>
+                    <span>{item.title}</span>
+                    <HiOutlineChevronDown className="w-5 h-5" />
+                    </button>
+                <div className={`absolute top-8 z-30 w-[250px] min-h-[300px] flex flex-col py-4 
+                bg-pruple-700 rounded-md ${transClass}`}>
+                    {
+                        MenuItems.map(item =>
+                            <Link
+                            key={item.route}
+                            className="transition duration-300 hover:scale-110 hover:text-purple-700 px-4 py-1"
+                            href={item?.route || ""}
+                             onClick={toggle}
+                            >{item.title}</Link>
+                        )
+                    }
+                </div>
+            </div>
+            {
+                isOpen ? <div className="fixed top-0 right-0 bottom-0 left-0 z-20 bg-black/40" onClick={toggle}></div> : <></>
+            }
+        </>
+    )
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,35 +1,64 @@
-"use client";
 import Link from "next/link";
 import { HiCog, HiOutlineChip, HiSearch, HiUser } from "react-icons/hi";
+import Dropdown from "./Dropdown";
+//import { prisma } from "@/lib/prisma";
 
+
+export interface MenuItem {
+    title: string;
+    route?: string;
+    children?: MenuItem[];
+}
+//Hacerlo dinamico
+export const MenuItem = {
+    title: "Tecnicas de recabacion",
+    children: [
+        { title: "Entrevista", route: "/entrevista" },
+        { title: "Encuesta", route: "/encuesta" },
+        { title: "Observación", route: "/observacion" },
+    ]
+};
 
 const Navbar = () => {
+    //trae datos usando prisma (en este momento (SOS)) - h
+    /*
+    Aqui no supe que rollo, mejor meti las opciones con codigo duro - h
+    const tecnicas =  await prisma.tecnica.findMany({
+        select: {nombre: true, id:true }
+    });
+    */
+
     return (
         <nav className="fixed top-0 w-full flex items-center
-        justify-around py-5 px-24 border-b border-gray-700 bg-white">
-            <Link href="/" className="flex items-center gap-3 text-purple-700 font-semibold
+        justify-between py-4 px-10 border-b border-gray-700 bg-white">
+            <div className="flex items-center gap-6">
+                <Link href="/" className="flex items-center gap-3 text-purple-700 font-semibold
             text-lg transition duration-300 hover:scale-110">
-                <HiOutlineChip className="w-16 h-16" />
-                <span className="text-2xl font-bold">TaskMaster+</span>
-            </Link>
-            
-           <ul className="flex gap-10 text-2xl">
+                    <HiOutlineChip className="w-16 h-16" />
+                    <span className="text-2xl font-bold">TaskMaster+</span>
+                </Link>
+                {/* Muestra el dropdown */}
+                <Dropdown item={MenuItem} />
+            </div>
+
+            <ul className="flex gap-10 text-2xl">
+
                 {/* Para buscar */}
                 <Link href="/" className="flex items-center gap-3 text-purple-700 font-semibold
                 text-lg transition duration-300 hover:scale-110">
-                    <HiSearch className="w-10 h-10"/>
+                    <HiSearch className="w-10 h-10" />
                 </Link>
                 {/* Configuracion */}
                 <Link href="/" className="flex items-center gap-3 text-purple-700 font-semibold
                 text-lg transition duration-300 hover:scale-110">
-                    <HiCog className="w-10 h-10"/>
+                    <HiCog className="w-10 h-10" />
                 </Link>
                 {/* Perfil */}
                 <Link href="/" className="flex items-center gap-3 text-purple-700 font-semibold
                 text-lg transition duration-300 hover:scale-110">
-                    <HiUser className="w-10 h-10"/>
+                    <HiUser className="w-10 h-10" />
                 </Link>
-           </ul>
+            </ul>
         </nav>
     )
 }
